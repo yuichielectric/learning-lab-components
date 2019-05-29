3d34f6e65ca6f5aa01dee33cf53fe4d8d1ed5d8d
James M. Greene <jamesmgreene@github.com>
2019-05-29 18:33:37 -0500

    Use 'describe.each' to test each action
    This results in more helpful output upon failures

--- tests/actions/index.test.js
+++ tests/actions/index.test.js
@@ -6,16 +6,13 @@ const actions = require('../../actions')
 const actionsDir = path.join(__dirname, '..', '..', 'actions')
 const actionsDocsDir = path.join(__dirname, '..', '..', 'docs', 'actions')
 
+let actionNames =
+  fs.readdirSync(actionsDir, { withFileTypes: true })
+    .filter(ent => ent.isDirectory())
+    .map(ent => ent.name)
+    .sort()
+
 describe('actions', () => {
-  let actionNames
-
-  beforeAll(() => {
-    actionNames = fs.readdirSync(actionsDir, { withFileTypes: true })
-      .filter(ent => ent.isDirectory())
-      .map(ent => ent.name)
-      .sort()
-  })
-
   it('module has properties for all expected actions', async () => {
     const actionKeys = Object.keys(actions).sort()
     expect(actionKeys).toEqual(actionNames)
@@ -43,5 +40,20 @@ describe('actions', () => {
     const expected = actionNames.map(name => `${name}.md`)
 
     expect(actionsDocsFiles).toEqual(expected)
+  })
+
+  describe.each(actionNames)('the action "%s"', (actionName) => {
+    it('has the minimum set of required files', async () => {
+      const actionDirFiles =
+        fs.readdirSync(path.join(actionsDir, actionName), { withFileTypes: true })
+          .filter(ent => ent.isFile())
+          .map(ent => ent.name)
+
+      expect(actionDirFiles).toContain('README.md')
+      expect(actionDirFiles).toContain('config.yml')
+      expect(actionDirFiles).toContain('index.js')
+      expect(actionDirFiles).toContain('schema.js')
+      expect(actionDirFiles).toContain(`${actionName}.test.js`)
+    })
   })
 })
