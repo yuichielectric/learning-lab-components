0387a97204854a14a89ba8d9760e5a7fe922f43a
Jason Etcovitch <jasonetco@github.com>
2019-06-05 23:09:55 -0400

    Remove tests for the action docs

--- tests/actions/index.test.js
+++ tests/actions/index.test.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 const actions = require('../../actions')
 
 const actionsDir = path.join(__dirname, '..', '..', 'actions')
-const actionsDocsDir = path.join(__dirname, '..', '..', 'docs', 'actions')
 
 let actionNames =
   fs.readdirSync(actionsDir, { withFileTypes: true })
@@ -17,29 +16,6 @@ describe('actions', () => {
     const actionKeys = Object.keys(actions).sort()
     expect(actionKeys).toEqual(actionNames)
     expect(actionKeys).toMatchSnapshot()
-  })
-
-  it('README mentions all expected actions', async () => {
-    const actionsReadme = fs.readFileSync(path.join(actionsDir, 'README.md'), { encoding: 'utf8' })
-    const expectedContents = `## Available actions
-
-<!-- START_ACTIONS_LIST -->
-` + actionNames.map(name => `* [${name}](./${name})`).join('\n') + `
-
-<!-- END_ACTIONS_LIST -->
-`
-    expect(actionsReadme).toContain(expectedContents)
-  })
-
-  it('associated documentation files exist for all expected actions', async () => {
-    const actionsDocsFiles = fs.readdirSync(actionsDocsDir, { withFileTypes: true })
-      .filter(ent => ent.isFile())
-      .map(ent => ent.name)
-      .sort()
-
-    const expected = actionNames.map(name => `${name}.md`)
-
-    expect(actionsDocsFiles).toEqual(expected)
   })
 
   describe.each(actionNames)('the action "%s"', (actionName) => {
